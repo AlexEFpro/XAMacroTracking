65b96f2a182b7e909d1bd19072c147198b49c128
AlexEFpro <alejandro.salinas@udgvirtual.udg.mx>
2025-08-17 16:09:28 -0600

    Initial Commit

---
+++ XAMacroTracking/XAMacroTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  XAMacroTrackingApp.swift
+//  XAMacroTracking
+//
+//  Created by Alejandro  Salinas Ramirez on 17/08/25.
+//
+
+import SwiftUI
+
+@main
+struct XAMacroTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
